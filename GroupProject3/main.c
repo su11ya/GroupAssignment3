64a718ee6529b75b9948d9c557ce78a979a2b023
Ya Su <suyawow@gmail.com>
2022-07-21 12:54:03 -0400

    Add project files.

---
+++ GroupProject3/main.c
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include "StackQueue.h"
+
+int main() {
+	LList Queue;
+	LList Stack;
+	//a)	Add the following values 6, 7, 8, 2, 5  to the queue
+	//b)	Add the following values 6, 7, 8, 2, 5  to the stack
+	//c)	Remove an element from stack
+	//d)	Remove an element from queue
+	//e)	Display the next element to be removed(do not remove) in the stack
+	//f)	Display the next element to be removed(do not remove) in the queue.*/
+
+
+
+
+}
