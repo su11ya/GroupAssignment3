66b87a5ba3e1c8d3125c230eadf80a3df4435b36
Ya Su <suyawow@gmail.com>
2022-07-21 12:55:50 -0400

    Add StackQueue.h

---
+++ GroupProject3/StackQueue.h
@@ -0,0 +1,45 @@
+#pragma once
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+typedef struct node {
+	int num; // the data in this node of the list
+	struct node* next; // the pointer to the next node
+} Node, * NodePtr;
+typedef struct llist {
+	NodePtr head;
+	NodePtr tail;
+} LList;
+
+// implementation of stack function 
+void push(int value, LList list) {
+
+
+}
+
+
+
+int  pop(LList list) {
+	if (list.head == NULL)
+		printf("empty");
+
+}
+// return the value to be removed
+int top(LList list) {
+
+}
+// implementation of the queue using singly linked list
+void enqueue(int value, LList list) {
+
+
+}
+int  dequeue(LList list) {
+	if (list.head)
+		printf("empty");
+
+}
+int peek(LList list) {
+
+}
+
+
+
