1d8ce7bb92d6e92d296fadd14da12286dcade495
Ya Su <suyawow@gmail.com>
2022-07-24 00:34:46 -0400

    Done - Stack push(); printList();

--- GroupProject3/StackQueue.h
+++ GroupProject3/StackQueue.h
@@ -3,45 +3,123 @@
 #include <stdio.h>
 #include <malloc.h>
 
+
 typedef struct node {
 	int num; // the data in this node of the list
 	struct node* next; // the pointer to the next node
 } Node, * NodePtr;
+
 typedef struct llist {
 	NodePtr head;
 	NodePtr tail;
 } LList;
 
+
+
+NodePtr makeNode(int n) {
+	NodePtr np = (NodePtr)malloc(sizeof(Node));
+	np->num = n;
+	np->next = NULL;
+	return np;
+}
+
+LList* initLList() {
+	//LList list;
+	LList* list = (LList*)malloc(sizeof(struct llist));
+	list->head = NULL;
+	list->tail = NULL;
+	return list;
+}
+
+//LList initLList() {
+//	LList np = (LList)malloc(sizeof(struct llist));
+//	
+//
+//}
+void printList(LList* list) {
+	NodePtr np = list->head;
+	//if (list.head == NULL) {
+	//	printf("\nEmpty");
+	//}
+	//else {
+	printf("\nElements present in the Stack: \n");
+	while (np != NULL) {
+		printf(" %d\n", np->num);
+		np = np->next;
+	}
+	/*printf("%d\n", list.head->num);*/
+//}
+
+}
+
 // implementation of stack function 
-void push(int value, LList list) {
+void push(int value, LList* list) {
+	NodePtr np = makeNode(value);
+	if (list->head == NULL) {
 
+		list->tail = np;
+	}
+	else {
+		np->next = list->head;
 
+	}
+	list->head = np;
+
+	//insert back
+	/*NodePtr np = makeNode(value);
+	if (list->head== NULL) {
+		list->head = np;
+	}
+	else {
+		list->tail->next = np;
+	}
+	list->tail = np;
+	*/
 }
 
-
-
-int  pop(LList list) {
-	if (list.head == NULL)
-		printf("empty");
-
-}
-// return the value to be removed
-int top(LList list) {
-
-}
-// implementation of the queue using singly linked list
-void enqueue(int value, LList list) {
-
-
-}
-int  dequeue(LList list) {
-	if (list.head)
-		printf("empty");
-
-}
-int peek(LList list) {
-
-}
-
+//int  pop(LList list) {
+//	NodePtr prev;
+//	NodePtr curr;
+//	if (list.head == NULL) {
+//		//if the list is empty
+//		printf("empty");
+//	}
+//	else {
+//		printf("\nRemoved element : % d", list.tail->num);
+//		prev = list.head;
+//		curr = list.head->next;
+//		while (curr != NULL) {
+//			prev = curr;
+//			curr = curr->next;
+//			if (curr->next = NULL) {
+//				prev->next = NULL;
+//				list.tail = prev;
+//				free(curr);
+//
+//			}
+//		}
+//
+//	}
+//
+//
+//}
+//// return the value to be removed
+//int top(LList list) {
+//
+//}
+//// implementation of the queue using singly linked list
+//void enqueue(int value, LList list) {
+//
+//
+//}
+//int  dequeue(LList list) {
+//	if (list.head)
+//		printf("empty");
+//
+//}
+//int peek(LList list) {
+//
+//}
+//
 
 
--- GroupProject3/main.c
+++ GroupProject3/main.c
@@ -2,17 +2,39 @@
 #include <stdio.h>
 #include "StackQueue.h"
 
+
 int main() {
 	LList Queue;
-	LList Stack;
+	//LList Stack 
 	//a)	Add the following values 6, 7, 8, 2, 5  to the queue
 	//b)	Add the following values 6, 7, 8, 2, 5  to the stack
+	NodePtr np = makeNode(60);
+	printf("\n%d", np->num);
+	LList* Stack = initLList();
+	push(6, Stack);
+	printList(Stack);
+	push(7, Stack);
+	printList(Stack);
+	push(8, Stack);
+	printList(Stack);
+	push(2, Stack);
+	printList(Stack);
+	push(5, Stack);
+	printList(Stack);
+	
+	
+	
+	
+
 	//c)	Remove an element from stack
+	//pop(Stack);
 	//d)	Remove an element from queue
 	//e)	Display the next element to be removed(do not remove) in the stack
+	//top(Stack);
 	//f)	Display the next element to be removed(do not remove) in the queue.*/
 
 
 
 
 }
+
